e27e4dbbb7871a1fbc5201dc88c616baec54a7a3
KMDMNAK <kmdmuneaki@gmail.com>
2020-04-18 12:54:28 +0900

    Complete test

--- src/acquise/kabutan.ts
+++ src/acquise/kabutan.ts
@@ -2,7 +2,7 @@
 import fetch from 'node-fetch';
 import execAll from 'execall'
 
-const columnCorrespond = {
+const columnCorrespond: { [key: string]: string } = {
     日付: "date",
     始値: "OR",
     高値: "HP",
@@ -13,24 +13,30 @@ const columnCorrespond = {
     "売買高(株)": "Rev"
 }
 
+const HEADER = {
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+    //"content-type": "application/json"
+}
 const PULL_DOWN_URL = "https://kabutan.jp/pulldown/";
-export const getPullDown = async (inputString: string) => {
-    const respJson = await fetch(PULL_DOWN_URL, {
+type PullDown = {
+    code: string,
+    name: string
+}
+export const getPullDown = async (inputString: string): Promise<PullDown[]> => {
+    const resp = await fetch(PULL_DOWN_URL, {
         method: 'POST',
         body: `input_string=${inputString}`,
-        headers: {
-            accept: "application/json, text/javascript",
-            "accept-encoding": "gzip, deflate, br",
-            "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-        }
+        headers: HEADER
     })
-    return respJson.map((each_text: string) => {
-        const codename = each_text.split(" ")
-        return { code: codename[0], name: codename[1] }
+    const body = await resp.text()
+    const bodyJson = (() => { try { return JSON.parse(body) } catch (e) { console.error(e); console.log(body); return [] } })()
+    return bodyJson.map((each_text: string) => {
+        const [code, name] = each_text.split(" ")
+        return { code, name }
     })
 }
-
-export const getStockData = async (code: string, type: string, page: Number) => {
+type DateType = 'day' | 'wek' | 'shin' | 'mon' | 'yar'
+export const getStockData = async (code: string, type: DateType, page: Number) => {
     const actualPage = page !== -1 ? page : 0
     const tableText = await getStockChartTable(code, type, actualPage)
     return extractData(tableText);
@@ -43,9 +49,9 @@ const getStockChartTable = async (code: string, type: string, page: Number) => {
     return getTable(respText)
 }
 
+const TBODY_PATTERN = /<table class="stock_kabuka[^0].?">[\s\S]*?<tbody>([\s\S]*?)<\/tbody>[\s\S]*?<\/table>/m;
 const getTable = (text: string) => {
-    const tbody_pattern = /<table class="stock_kabuka[^0].?">[\s\S]*?<tbody>([\s\S]*?)<\/tbody>[\s\S]*?<\/table>/m;
-    const matchObject = tbody_pattern.exec(text);
+    const matchObject = TBODY_PATTERN.exec(text);
     if (matchObject === null) throw Error("no match exception")
     const table = matchObject[0]
     return table;
@@ -63,19 +69,16 @@ const extractData = (tableText: string) => {
     if (trs === null) throw Error("no match in tr error");
     //一番最初は項目である前提でコラムを作成
     const columns = createColumn(trs[0])
-    const result = trs.slice(1).map((each_tr, i) => {
+    return trs.slice(1).map((each_tr) => {
         const each_data = execAll(DATA_PATTERN, each_tr.match);
         if (each_data === null) return;
-        const data: { [Key: string]: any } = {}
-        columns.forEach((column, i) => {
-            const each_match = each_data[i]
-            data[columnCorrespond[column]] = column === "日付" ? each_match.subMatches[0] : +each_match.subMatches[0].replace(/,/g, '')
-        })
-        return data
+        return columns.reduce((data, columnName, index) => {
+            const each_match = each_data[index]
+            data[columnCorrespond[columnName]] = columnName === "日付" ? each_match.subMatches[0] : +each_match.subMatches[0].replace(/,/g, '')
+            return data
+        }, {} as { [key: string]: string | number })
     })
-    return result;
 }
-
 
 
 
