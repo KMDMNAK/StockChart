04d7c1032ef20f6dc26d29c401a2d614c84c3309
KMDMNAK <kmdmuneaki@gmail.com>
2020-04-18 18:40:44 +0900

    change configs

--- config/env.bat
+++ config/env.bat
@@ -1,2 +1,5 @@
+set PROJECT_ID=stockchart
+set GCLOUD_PROJECT=%PROJECT_ID%
 echo "You should execute this script from root directory."
 set QUANDL_CONFIG_PATH=%CD%\environment\quandl.config.json
+set GOOGLE_APPLICATION_CREDENTIALS=%CD%\environment\%PROJECT_ID%-sa.json
