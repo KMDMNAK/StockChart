5f10af92fe91b71c315ca5fbae85ee3449d865c8
KMDMNAK <kmdmuneaki@gmail.com>
2020-06-07 23:18:30 +0900

    utils

---
+++ client/src/tests/CompanyChart.test.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+/**
+ * Have to check
+ * CompanyDataContext
+ * AttrContext
+ * 
+ * Component
+ *  Chart
+ */
+
+ // 初期表示
+ // reload時
+ 
+describe('Hello', () => {
+    it('Show', () => {
+        console.log('hello world')
+    })
+})
---
+++ client/src/utils/firebase.ts
@@ -0,0 +1,5 @@
+import * as firebase from 'firebase';
+import { COMPANY_REF, DAILY_UPCOMPANY_DOCREF, DAILY_DOWNCOMPANY_DOCREF, DAILY_REF } from '../constants'
+export const fstore = firebase.firestore()
+export const upCompaniesRef = fstore.collection(DAILY_REF).doc(DAILY_UPCOMPANY_DOCREF)
+export const downCompaniesRef = fstore.collection(DAILY_REF).doc(DAILY_DOWNCOMPANY_DOCREF)
