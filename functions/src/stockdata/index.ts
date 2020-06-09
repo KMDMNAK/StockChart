0f29bb13748d93479577fa4559829d92486b051c
KMDMNAK <kmdmuneaki@gmail.com>
2020-06-09 23:31:07 +0900

    developing

--- functions/src/stockdata/index.ts
+++ functions/src/stockdata/index.ts
@@ -9,3 +9,13 @@ export const updateCompanyData = async () => {
         const companyName = doc.id
     })
 }
+
+//
+export const a = async (companyName: string) => {
+    const companySnapshot = await fstore.collection(COMPANY_REF).doc(companyName).get()
+    const data = companySnapshot.data() as { lastDate: string }
+    const lastDate = data.lastDate
+    const date = new Date()
+    const todayString = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+    todayString
+}
