7e8ea34302c94c81239708d47cecce060ef59ee9
KMDMNAK <kmdmuneaki@gmail.com>
2020-06-21 07:32:16 +0900

    Fix Layout

--- client/src/pages/detail.tsx
+++ client/src/pages/detail.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CompanyChart from '../component/CompanyChart'
 import SearchCompany from '../component/SearchCompany'
 import CompanyNews from '../component/CompanyNews'
@@ -6,11 +6,19 @@ import CompanyNews from '../component/CompanyNews'
 export default (props: { setCompany: (companyName: string) => any, companyName: string }) => {
     // const [title, url] = [`${props.companyName}`, `/detail/${props.companyName}`]
     // history.pushState({ title: title, url: url }, title, url)
+    const [companyName, setCompanyName] = useState(props.companyName)
     return (<div style={pageStyle}>
-        <h2>{decodeURI(props.companyName)}</h2>
-        <SearchCompany decideCompany={(companyName: string) => props.setCompany(companyName)} />
-        <CompanyChart companyName={props.companyName} />
-        <CompanyNews companyName={props.companyName} />
+        <SearchCompany decideCompany={(companyName: string) => {
+            props.setCompany(companyName);
+            setCompanyName(companyName)
+        }} />
+        <h2>{decodeURI(companyName)}</h2>
+        <div style={{ margin: 40, textAlign: 'center' }}>
+            <CompanyChart companyName={companyName} />
+        </div>
+        <div style={{ margin: '50px' }}>
+            <CompanyNews companyName={companyName} />
+        </div>
     </div>)
 }
 
