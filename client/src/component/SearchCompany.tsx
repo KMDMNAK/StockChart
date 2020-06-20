8a6c53aea8b1fe61f869651a98783630c3c47d4a
KMDMNAK <kmdmuneaki@gmail.com>
2020-06-21 07:31:58 +0900

    Add Support for pull down for search prediction

--- client/src/component/SearchCompany.tsx
+++ client/src/component/SearchCompany.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, KeyboardEvent } from 'react'
 import { hot } from 'react-hot-loader/root'
 import { searchCompanyName } from '../firebase'
 
 type PredictHandlerState = { inputWord: string, predictValues: string[] }
 const PredictContext = React.createContext({ inputWord: '', predictValues: [] } as PredictHandlerState)
-const PredictHandler = (props: { children: JSX.Element }) => {
+const PredictHandler = (props: { children: React.ReactNode, decideCompany: (inputWord: string) => any }) => {
     const [predictInfo, setPredictInfo] = useState({ inputWord: '', predictValues: [] } as PredictHandlerState)
     console.debug('PredictHandler', { predictInfo })
     return (
@@ -12,7 +12,13 @@ const PredictHandler = (props: { children: JSX.Element }) => {
             <label>
                 <input type="text" onChange={async (event: React.ChangeEvent<HTMLInputElement>) => {
                     await changeInputWord(event.target.value, setPredictInfo)
-                }} />
+                }} autoComplete="on" list="companies"
+                    onKeyDown={(event: KeyboardEvent) => {
+                        if (event.key === 'Enter') {
+                            props.decideCompany(predictInfo.inputWord)
+                        }
+                    }}
+                />
             </label>
             <PredictContext.Provider value={predictInfo}>
                 {props.children}
@@ -28,14 +34,16 @@ const changeInputWord = async (inputWord: string, setPredictInfo: (info: Predict
 
 const SearchCompany = (props: { decideCompany: (inputWord: string) => any }) => {
     return (
-        <PredictHandler>
-            <PredictContext.Consumer>
-                {(predictInfo) => predictInfo.predictValues.map(companyName => (
-                    <div key={companyName} onClick={() => props.decideCompany(companyName)}>
-                        {companyName}
-                    </div>))
-                }
-            </PredictContext.Consumer>
+        <PredictHandler decideCompany={props.decideCompany}>
+            <datalist id="companies">
+                <PredictContext.Consumer>
+                    {(predictInfo) => predictInfo.predictValues.map(companyName => (
+                        <option key={companyName} style={{ margin: 10 }}>
+                            {companyName}
+                        </option>))
+                    }
+                </PredictContext.Consumer>
+            </datalist>
         </PredictHandler>)
 }
 
